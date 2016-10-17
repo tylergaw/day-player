854e1dad2959ed10ba9d10653d272beaa1dacf32
Tyler Gaw <me@tylergaw.com>
2016-10-16 23:33:21 -0400

    Adds placehold.it custom urlBuilder so it works

--- src/handlers/placeHoldIt.js
+++ src/handlers/placeHoldIt.js
@@ -1,10 +1,38 @@
 // eslint-disable-next-line no-unused-vars
 const placeHoldIt = createPluginHandler(function(props) {
-  const alert = new Alert({
-    message: 'Placehold.it Options',
-    info: 'Customize the image that will be created.',
-    iconUrl: props.api.resourceNamed('placeholdit.icns')
+  const initOpts = props.newImgFrame;
+
+  const elements = [
+    new Label({
+      value: 'Width:'
+    }),
+    new TextField({
+      name: 'width',
+      value: initOpts.width
+    }),
+    new Label({
+      value: 'Height:'
+    }),
+    new TextField({
+      name: 'height',
+      value: initOpts.height
+    })
+  ];
+
+  const onConfirm = createConfirmHandler({
+    api: props.api,
+    group: props.target.group,
+    host: 'placehold.it',
+    initOpts: initOpts,
+    urlBuilder: function(parts) {
+      return `${parts.protocol}${parts.host}/${parts.width}x${parts.height}`;
+    }
   });
 
-  alert.runModal();
+  new Alert({
+    message: 'Placehold.it Options',
+    info: 'Customize the image that will be created.',
+    iconUrl: props.api.resourceNamed('placeholdit.icns'),
+    onConfirm: onConfirm
+  }).append(elements).runModal();
 });
