f120d86231681cf817b1f14dfd7569ba202ba81e
Tyler Gaw <me@tylergaw.com>
2016-10-16 22:46:54 -0400

    Sets up Place Cage with standard Day Player

--- src/handlers/placeCage.js
+++ src/handlers/placeCage.js
@@ -1,10 +1,35 @@
 // eslint-disable-next-line no-unused-vars
 const placeCage = createPluginHandler(function(props) {
-  const alert = new Alert({
-    message: 'PlaceCage Options',
-    info: 'Customize the image of the best actor of all time that will be created.',
-    iconUrl: props.api.resourceNamed('placecage.icns')
+  const initOpts = props.newImgFrame;
+
+  const elements = [
+    new Label({
+      value: 'Width:'
+    }),
+    new TextField({
+      name: 'width',
+      value: initOpts.width
+    }),
+    new Label({
+      value: 'Height:'
+    }),
+    new TextField({
+      name: 'height',
+      value: initOpts.height
+    })
+  ];
+
+  const onConfirm = createConfirmHandler({
+    api: props.api,
+    group: props.target.group,
+    host: 'placecage.com',
+    initOpts: initOpts
   });
 
-  alert.runModal();
+  new Alert({
+    message: 'Place Cage Options',
+    info: 'Customize the image of the best actor of all time that will be created.',
+    iconUrl: props.api.resourceNamed('placecage.icns'),
+    onConfirm: onConfirm
+  }).append(elements).runModal();
 });
