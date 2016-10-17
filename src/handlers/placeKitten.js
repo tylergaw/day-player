d17786a870f8341f526ffcfd222f69a06be0fd45
Tyler Gaw <me@tylergaw.com>
2016-10-16 23:45:40 -0400

    Adds placekitten and unsplashit with standard setup

--- src/handlers/placeKitten.js
+++ src/handlers/placeKitten.js
@@ -1,10 +1,35 @@
 // eslint-disable-next-line no-unused-vars
 const placeKitten = createPluginHandler(function(props) {
-  const alert = new Alert({
-    message: 'Place Kitten Options',
-    info: 'Customize the kewl kitten image that will be created.',
-    iconUrl: props.api.resourceNamed('placekitten.icns')
+  const initOpts = props.newImgFrame;
+
+  const elements = [
+    new Label({
+      value: 'Width:'
+    }),
+    new TextField({
+      name: 'width',
+      value: initOpts.width
+    }),
+    new Label({
+      value: 'Height:'
+    }),
+    new TextField({
+      name: 'height',
+      value: initOpts.height
+    })
+  ];
+
+  const onConfirm = createConfirmHandler({
+    api: props.api,
+    group: props.target.group,
+    host: 'placekitten.com',
+    initOpts: initOpts
   });
 
-  alert.runModal();
+  new Alert({
+    message: 'Place Kitten Options',
+    info: 'Customize the kewl kitten image that will be created.',
+    iconUrl: props.api.resourceNamed('placekitten.icns'),
+    onConfirm: onConfirm
+  }).append(elements).runModal();
 });
--- src/handlers/unsplashIt.js
+++ src/handlers/unsplashIt.js
@@ -1,10 +1,35 @@
 // eslint-disable-next-line no-unused-vars
 const unsplashIt = createPluginHandler(function(props) {
-  const alert = new Alert({
-    message: 'Unsplash.it Options',
-    info: 'Customize the image that will be created.',
-    iconUrl: props.api.resourceNamed('unsplashit.icns')
+  const initOpts = props.newImgFrame;
+
+  const elements = [
+    new Label({
+      value: 'Width:'
+    }),
+    new TextField({
+      name: 'width',
+      value: initOpts.width
+    }),
+    new Label({
+      value: 'Height:'
+    }),
+    new TextField({
+      name: 'height',
+      value: initOpts.height
+    })
+  ];
+
+  const onConfirm = createConfirmHandler({
+    api: props.api,
+    group: props.target.group,
+    host: 'unsplash.it',
+    initOpts: initOpts
   });
 
-  alert.runModal();
+  new Alert({
+    message: 'Unsplash.it Options',
+    info: 'Customize the image that will be created.',
+    iconUrl: props.api.resourceNamed('unsplashit.icns'),
+    onConfirm: onConfirm
+  }).append(elements).runModal();
 });
