029d59d2ecd85a24ccf15f4616fa63f68a494aef
Tyler Gaw <me@tylergaw.com>
2016-10-30 00:58:24 -0400

    Adds blur and type options and random default to unsplash

--- src/handlers/unsplashIt.js
+++ src/handlers/unsplashIt.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 const unsplashIt = createPluginHandler(function(props) {
+  const GREY_TITLE = 'Black & white';
+  const BLUR_TITLE = 'Blurry';
   const initOpts = props.newImgFrame;
 
   const elements = [
@@ -16,6 +18,20 @@ const unsplashIt = createPluginHandler(function(props) {
     new TextField({
       name: 'height',
       value: initOpts.height
+    }),
+    new Label({
+      value: 'Type:'
+    }),
+    new PopUpButton({
+      name: 'type',
+      items: ['Color', GREY_TITLE]
+    }),
+    new Label({
+      value: 'Sharpness:'
+    }),
+    new PopUpButton({
+      name: 'blur',
+      items: ['Focused', BLUR_TITLE]
     })
   ];
 
@@ -23,7 +39,14 @@ const unsplashIt = createPluginHandler(function(props) {
     api: props.api,
     group: props.target.group,
     host: 'unsplash.it',
-    initOpts: initOpts
+    initOpts: initOpts,
+    urlBuilder: function(parts) {
+      const base = `${parts.protocol}${parts.host}`;
+      // Cast as a string because the value coming back is an object
+      const type = (String(parts.allParts.type) === GREY_TITLE) ? '/g' : '';
+      const blur = (String(parts.allParts.blur) === BLUR_TITLE) ? '&blur' : '';
+      return `${base}${type}/${parts.width}/${parts.height}?random${blur}`;
+    }
   });
 
   new Alert({
